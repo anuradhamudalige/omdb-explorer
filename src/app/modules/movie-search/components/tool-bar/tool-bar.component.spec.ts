0e0271da903f6ad1870b768d0ad3d3770b2d0668
Anuradha Mudalige <anuradamudalige@gmail.com>
2021-04-01 05:32:46 +0530

    OMDB | Added more unit tests

--- src/app/modules/movie-search/components/tool-bar/tool-bar.component.spec.ts
+++ src/app/modules/movie-search/components/tool-bar/tool-bar.component.spec.ts
@@ -6,6 +6,7 @@ import { MessageService } from '../../../shared-module/services/message.service'
 describe('ToolBarComponent', () => {
   let component: ToolBarComponent;
   let fixture: ComponentFixture<ToolBarComponent>;
+  let messageService: MessageService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -19,9 +20,30 @@ describe('ToolBarComponent', () => {
     fixture = TestBed.createComponent(ToolBarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    messageService = fixture.debugElement.injector.get(MessageService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should clear the messages when proper input is provided', () => {
+    spyOn(messageService, 'clear');
+    const sub = component.search.subscribe(value => {
+      expect(value).toEqual('some_title');
+    });
+
+    component.onSearchClicked('some_title');
+
+    expect(messageService.clear).toHaveBeenCalled();
+    sub.unsubscribe();
+  });
+
+  it('should not clear the messages when proper input is not provided', () => {
+    spyOn(messageService, 'clear');
+    component.onSearchClicked('');
+
+    expect(messageService.clear).not.toHaveBeenCalled();
   });
 });
