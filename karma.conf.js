f88e40fd9e8cb090eff587cc1302242a1499c2e2
Anuradha Mudalige <anuradamudalige@gmail.com>
2021-04-25 10:35:33 +0530

    Removed specifics

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/omdb-rentman'),
+      dir: require('path').join(__dirname, './coverage/omdb-explorer'),
       subdir: '.',
       reporters: [
         { type: 'html' },
